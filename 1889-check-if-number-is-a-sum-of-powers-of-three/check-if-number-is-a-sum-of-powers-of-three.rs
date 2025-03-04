413191260cb92c783178dd935bf4f87483f658fc
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2025-03-04 10:42:12 +0100

    Time: 0 ms (100.00%) | Memory: 2.2 MB (52.94%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn check_powers_of_three(n: i32) -> bool {
+        
+        Solution::conv(n)
+
+    }
+
+    pub fn conv(mut n:i32) -> bool{
+
+        if n == 0 {
+            return true
+        }
+
+        let r = n % 3;
+
+        n = n/3;
+
+        if r < 0 {
+            n += 1;
+        }
+
+        return r != 2  && Solution::conv(n)
+    }
+
+}
